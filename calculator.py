8d80fc3a17c60db5ca04c2820fb370d49e2a1b7f
shirish0123 <shirishlikesramen@gmail.com>
2024-12-16 10:11:59 +0545

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a,b,c = 1,2,3
+sum= a+b
+diff=b-a
+product_result=a*b
+division_result=b/a
+modulus=b%a
+power_result=a**b
+floor_division_result= b//a
+
+print(sum)
+print(diff)
+print(product_result)
+print(division_result)
+print(modulus)
+print(power_result)
+print(floor_division_result)
